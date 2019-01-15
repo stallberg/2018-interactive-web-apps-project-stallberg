d5c7b8eb0794ba5a506cfda9a50600a347ad32dc
stallberg <stallber@abo.fi>
2019-01-15 20:28:19 +0200

    latest

---
+++ stock-portfolio/build/precache-manifest.16818ba42f00a94e164531e5405998c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e03b952df224d66ec6ec",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/static/js/runtime~main.e03b952d.js"
+  },
+  {
+    "revision": "d213e466a1312f028d05",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/static/js/main.d213e466.chunk.js"
+  },
+  {
+    "revision": "15190afa457cd6da443d",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/static/js/1.15190afa.chunk.js"
+  },
+  {
+    "revision": "d213e466a1312f028d05",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/static/css/main.c181770e.chunk.css"
+  },
+  {
+    "revision": "15190afa457cd6da443d",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/static/css/1.e7bb38bf.chunk.css"
+  },
+  {
+    "revision": "dfbfe4b212c3ee3cb395af42c99c152e",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-stallberg/index.html"
+  }
+];
