94472759d872ce881d4c35e9f7a72a5afb4b8a03
stallberg <stallber@abo.fi>
2019-01-15 23:13:48 +0200

    Updates

---
+++ precache-manifest.d5fe27badf96060db44e25fc1132a9ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1cde0743791f6923d4d",
+    "url": "/2018-interactive-web-apps-project-stallberg/static/js/runtime~main.f1cde074.js"
+  },
+  {
+    "revision": "2fe338a3d191357c4f61",
+    "url": "/2018-interactive-web-apps-project-stallberg/static/js/main.2fe338a3.chunk.js"
+  },
+  {
+    "revision": "a6cc787b82f2509730f3",
+    "url": "/2018-interactive-web-apps-project-stallberg/static/js/1.a6cc787b.chunk.js"
+  },
+  {
+    "revision": "2fe338a3d191357c4f61",
+    "url": "/2018-interactive-web-apps-project-stallberg/static/css/main.788a3291.chunk.css"
+  },
+  {
+    "revision": "a6cc787b82f2509730f3",
+    "url": "/2018-interactive-web-apps-project-stallberg/static/css/1.fd69b392.chunk.css"
+  },
+  {
+    "revision": "7297e81222b3511e7677b68ea773ee4b",
+    "url": "/2018-interactive-web-apps-project-stallberg/index.html"
+  }
+];
